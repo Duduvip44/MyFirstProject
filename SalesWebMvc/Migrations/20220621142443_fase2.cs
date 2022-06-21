ce54aaa7c7e21eb060d96755e3d091f571f747f1
Eduardo Sousa <essousa7@gmail.com>
2022-06-21 11:27:18 -0300

    fase2

---
+++ SalesWebMvc/Migrations/20220621142443_fase2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class fase2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -13,7 +13,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         
         public static Department Department { get; set; }
-        public int DepartmentId { get; set; }
+       
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
